2fa0c5ea60f6ebeb1dc425c0f7ae7be9e3d71442
John E. Rollinson <j.e.rollinson@gmail.com>
2020-07-09 13:01:16 -0400

    Revert temporary fix

--- cmgr/filesystem.go
+++ cmgr/filesystem.go
@@ -240,6 +240,7 @@ func checksumIgnore(name string) bool {
 	return (name[0] == '.' && name != ".dockerignore") ||
 		name == "README" ||
 		name == "README.md" ||
+		name == "problem.json" ||
 		name == "problem.md" ||
 		name == "solver" ||
 		name == "cmgr.db"
