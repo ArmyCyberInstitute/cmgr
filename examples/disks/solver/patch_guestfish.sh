e72c03924d4fd477b315f28148df91ffb0170a10
John E. Rollinson <john.rollinson@westpoint.edu>
2021-02-05 16:25:03 -0500

    Fix disks example

--- examples/disks/solver/patch_guestfish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This is a dirty patch to undo a recent patch in libguestfs that prevents
-# degraded array from being mounted inside of a guestfish session.
-
-/usr/bin/tar xf /usr/lib/x86_64-linux-gnu/guestfs/supermin.d/init.tar.gz
-/usr/bin/sed -i "s|^mdadm.*|mdadm --run /dev/md127|" init
-/usr/bin/tar czvf init.tar.gz init
-/usr/bin/mv init.tar.gz /usr/lib/x86_64-linux-gnu/guestfs/supermin.d/init.tar.gz
--- examples/disks/solver/solve.py
+++ examples/disks/solver/solve.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import subprocess
 
-subprocess.run(["/bin/sh","patch_guestfish.sh"])
 subprocess.run(["tar","xf","disks.tar.gz"])
 subprocess.run(["/usr/bin/guestfish","-f","recover_flag.gfsh"])
